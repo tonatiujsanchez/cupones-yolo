c0fe1546cffba101985457a5014cd8536c9bf275
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-01-18 13:39:27 -0600

    Endpoint para obtener rutas añadido

---
+++ src/pages/api/public/routes.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/database'
+import { IRoute } from '@/interfaces'
+import { Route } from '@/models'
+
+type Data = 
+    | { msg: string }
+    | IRoute[]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        
+        case 'GET':
+            return getRoutes( res, res )
+                
+        default:
+            return res.status(400).json({ msg: 'Bad Request' })
+    }
+}
+
+const getRoutes = async(res: NextApiResponse<Data>, res1: NextApiResponse<Data>) => {
+    
+    try {
+        await db.connect()
+        const routes = await Route.find({ status: true, active: true })
+        await db.disconnect()
+        
+        return res.status(200).json( routes )
+    } catch (error) {
+        await db.disconnect()
+        console.log(error)
+        return res.status(500).json({ msg: 'Error en el servidor, comuníquese con el administrador' })
+    }
+
+}
--- src/pages/api/seed.ts
+++ src/pages/api/seed.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/database'
+import { Route } from '@/models'
 
 type Data = {
     msg: string
@@ -10,6 +12,14 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
     if( process.env.NODE_ENV === 'production' ){
         return res.status(401).json({ msg: 'Not authorized' })
     }
+
+    await  db.connect()
+    const newRoute = new Route({
+        title: 'Cupones',
+        slug : 'cupones',
+    })
+    await newRoute.save()
+    await db.disconnect()
 
     return res.status(200).json({ msg: 'ok!' })
 }
